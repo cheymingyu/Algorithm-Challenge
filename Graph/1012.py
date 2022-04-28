494716af0ab8147becd5c09143d9109743fa99f6
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2022-04-28 22:37:14 +0900

    220428

--- Graph/1012.py
+++ Graph/1012.py
@@ -5,6 +5,7 @@
 from collections import deque
 
 def bfs(graph, a, b, m, n):
+    # 탐색에 사용
     dx = [1, 0, -1, 0]
     dy = [0, 1, 0, -1]
     queue = deque()
@@ -16,9 +17,11 @@ def bfs(graph, a, b, m, n):
         for i in range(4):
             new_x = x + dx[i]
             new_y = y + dy[i]
+            # index 검사
             if new_x < 0 or new_x >=n or new_y < 0 or new_y >= m:
                 continue
             if ground[new_x][new_y] == 1:
+                # 값이 1이면 큐에 집어넣고 0으로 바꿈
                 queue.append((new_x, new_y))
                 ground[new_x][new_y] = 0
     
@@ -28,6 +31,7 @@ T = int(input())
 
 for i in range(T):
     M, N, K = map(int, input().split())
+    # M * N 배열 생성
     ground = [[0] * M for _ in range(N)]
     count = 0
     
@@ -39,6 +43,7 @@ for i in range(T):
         for y in range(M):
             if ground[x][y] == 1:
                 bfs(ground, x, y, M, N)
+                # bfs 하고 나면 현재 x, y 주변을 모두 탐색했으므로 count += 1
                 count += 1
     print(count)
     
