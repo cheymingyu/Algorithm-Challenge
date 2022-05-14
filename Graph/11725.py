9226da955d6464d1bac87f569c8b16b3cc54d331
Mingyu Choi <cmg5691@g.cnu.ac.kr>
2022-05-14 22:11:47 +0900

    2220514

--- Graph/11725.py
+++ Graph/11725.py
@@ -1,5 +1,5 @@
 # 시작시간: 2022.05.14 19:10
-# 종료시간: 2022.05.14
+# 종료시간: 2022.05.14 22:11
 # 백준 11725번 https://www.acmicpc.net/proablem/11725
 # 트리의 부모 찾기
 
@@ -21,18 +21,18 @@ root = list(-1 for _ in range(N+1))
 def bfs():
     global graph, N, root
     queue = deque()
-    # queue에 원소를 두개씩 넣고 돌렸더니 93%에서 시간초과가 발생함
-    for c in graph[1]:
-        queue.append((1, c))
+    # queue의 원소개수를 1개로 줄였더니 잘 돌아감.
+    queue.append(1)
     while queue:
-        parent, child = queue.popleft()
-        root[child] = parent
-        for c in graph[child]:
+        now = queue.popleft()
+        for c in graph[now]:
             if root[c] == -1:
-                queue.append((child, c))
+                root[c] = now
+                queue.append(c)
 
 bfs()
 s = ""
+
 for r in root[2:]:
     s += (str(r) + '\n')
 print(s)
